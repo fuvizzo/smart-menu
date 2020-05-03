ae611b14d5d85b187652a7c329b7a7417324a307
Fulvio Cusimano <fulvio.cusimano@gmail.com>
2020-05-03 16:34:20 +0200

    Added menu item edit function

--- src/Components/OwnerAdmin/index.js
+++ src/Components/OwnerAdmin/index.js
@@ -8,7 +8,38 @@ function OwnerAdmin(props) {
   const path = `/list/${id}/menu`;
   const [menu, setMenu] = useState(new Map());
 
-  const toggleEditMode = useCallback(menuItemId => {});
+  const defaultEditModeState = {
+    enabled: false,
+    selectedItem: {},
+  };
+  const [editModeState, setEditModeState] = useState(defaultEditModeState);
+
+  const onChangeValue = useCallback(
+    event => {
+      const input = event.currentTarget;
+      const currentValue = input.value;
+      const selectedItem = editModeState.selectedItem;
+      selectedItem.value[input.name] = currentValue;
+      setEditModeState({
+        enabled: true,
+        selectedItem,
+      });
+    },
+    [editModeState.selectedItem]
+  );
+
+  const toggleEditMode = useCallback(
+    menuItemId => {
+      setEditModeState({
+        enabled: true,
+        selectedItem: {
+          id: menuItemId,
+          value: menu.get(menuItemId),
+        },
+      });
+    },
+    [menu]
+  );
 
   const deleteMenuItemCallback = useCallback(
     menuItemId => {
@@ -29,14 +60,34 @@ function OwnerAdmin(props) {
     [firebaseService, path, menu]
   );
 
-  const updateMenuItemCallback = useCallback(() => {});
+  const updateMenuItemCallback = useCallback(() => {
+    const menuItemId = editModeState.selectedItem.id;
+
+    const updateMenuItem = async () => {
+      const data = {
+        path: `${path}/${menuItemId}`,
+        body: editModeState.selectedItem.value,
+      };
+      try {
+        await firebaseService.update(data);
+
+        setEditModeState({
+          enabled: false,
+          selectedItem: {},
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    updateMenuItem();
+  }, [editModeState.selectedItem.value, editModeState.selectedItem.id]);
 
   const createMenuItemCallback = useCallback(() => {
     const createMenuItem = async () => {
       const menuItemId = uuidv1();
 
       const data = {
-        path: `${path}/${menuItemId}}`,
+        path: `${path}/${menuItemId}`,
         body: {
           category: 'Dessert',
           description: 'desc',
@@ -84,15 +135,75 @@ function OwnerAdmin(props) {
           const data = menu.get(key);
           return (
             <li key={key}>
-              <div> {data.category}</div>
-              <div> {data.name}</div>
-              <div> {data.descriptions}</div>
-              <div> {data.ingredients}</div>
-              <div> {data.price}</div>
-              <button onClick={() => deleteMenuItemCallback(key)}>
-                Delete
-              </button>
-              <button onClick={() => toggleEditMode(key)}>Edit</button>
+              {editModeState.selectedItem.id === key &&
+              editModeState.enabled ? (
+                <div>
+                  Edit mode
+                  <div>
+                    <div>
+                      <input
+                        name="category"
+                        onChange={onChangeValue}
+                        type="text"
+                        placeholder="Category"
+                        value={data.category}
+                      />
+                    </div>
+                    <div>
+                      <input
+                        name="name"
+                        onChange={onChangeValue}
+                        type="text"
+                        placeholder="Name"
+                        value={data.name}
+                      />
+                    </div>
+                    <div>
+                      <input
+                        name="description"
+                        onChange={onChangeValue}
+                        type="text"
+                        placeholder="Description"
+                        value={data.description}
+                      />
+                    </div>
+                    <div>
+                      <input
+                        name="ingredients"
+                        onChange={onChangeValue}
+                        type="text"
+                        placeholder="Ingredients"
+                        value={data.ingredients}
+                      />
+                    </div>
+                    <div>
+                      <input
+                        name="price"
+                        onChange={onChangeValue}
+                        type="text"
+                        placeholder="Price"
+                        value={data.price}
+                      />
+                    </div>
+
+                    <button onClick={() => updateMenuItemCallback()}>
+                      Apply changes
+                    </button>
+                  </div>
+                </div>
+              ) : (
+                <div>
+                  <div> {data.category}</div>
+                  <div> {data.name}</div>
+                  <div> {data.description}</div>
+                  <div> {data.ingredients}</div>
+                  <div> {data.price}</div>
+                  <button onClick={() => deleteMenuItemCallback(key)}>
+                    Delete
+                  </button>
+                  <button onClick={() => toggleEditMode(key)}>Edit</button>
+                </div>
+              )}
             </li>
           );
         })}
