dc0b1916fded16f3d92aa4be3505b60c445f2255
Fulvio Cusimano <fulvio.cusimano@gmail.com>
2020-06-03 11:29:15 +0200

    firebase sdk for production/ bug fix for supporting client-side routing

--- src/Firebase/index.js
+++ src/Firebase/index.js
@@ -1,7 +1,10 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
 import firebaseMock from '../Mock/firebase-mock';
 
-import FirebaseService from './firebase-service';
+import 'firebase/database';
+import 'firebase/firebase-storage';
+import 'firebase/firebase-auth';
+
 const firebaseConfig = {
   apiKey: 'AIzaSyApy1VB9ENzXTjyABftKFf0t7DpF5itsRw',
   authDomain: 'smart-menu-794bf.firebaseapp.com',
@@ -14,4 +17,63 @@ const firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-export default new FirebaseService(firebase);
+class FirebaseService {
+  #database = null;
+  #storage = null;
+
+  constructor() {
+    this.#database = firebase.database();
+    this.#storage = firebase.storage();
+    this.auth = firebase.auth();
+
+    this.auth.onAuthStateChanged(user => {
+      // console.log(user);
+    });
+  }
+
+  auth = this.auth;
+
+  storage = {
+    getDownloadURL: async path => {
+      const resource = await this.#storage.ref(path);
+      return await resource.getDownloadURL();
+    },
+
+    deleteFile: async path => this.#storage.ref(path).delete(),
+
+    uploadFile: async (path, file, onUploading, metadata = null) => {
+      const resource = this.#storage.ref(path);
+      const uploadTask = metadata
+        ? resource.put(file, metadata)
+        : resource.put(file);
+      uploadTask.on('state_changed', onUploading);
+      return await uploadTask;
+    },
+  };
+  database = {
+    create: async ({ path, body }) => this.#database.ref(path).set(body),
+
+    delete: async path => this.#database.ref(path).remove(),
+
+    update: async ({ path, body }) => this.#database.ref(path).update(body),
+
+    read: async path => this.#database.ref(path).once('value'),
+
+    orderByChild: (path, name, value) => {
+      const orderByChild = this.#database
+        .ref(path)
+        .orderByChild(name)
+        .equalTo(value);
+      return {
+        read: async () => await orderByChild.once('value'),
+      };
+    },
+
+    orderByValue: async (path, value) => {
+      const x = this.#database.ref(path).orderByValue();
+      return x.equalTo(value).once('value');
+    },
+  };
+}
+
+export default new FirebaseService();
