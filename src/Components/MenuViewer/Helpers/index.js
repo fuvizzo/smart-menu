3a95f006b2aad71d7b4444bc260de6a6c9c0c08b
Fulvio Cusimano <fulvio.cusimano@gmail.com>
2020-06-10 11:12:13 +0200

    fixing missing image and icon for Pizza menu type

--- src/Components/MenuViewer/Helpers/index.js
+++ src/Components/MenuViewer/Helpers/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { LocalBar, RestaurantMenu } from '@material-ui/icons';
+import { LocalBar, RestaurantMenu, LocalPizza } from '@material-ui/icons';
 import SvgIcon from '@material-ui/core/SvgIcon';
 import FOOD_AND_DRINKS_IMAGE from '../../../Assets/FOOD_AND_DRINKS.jpg';
 import WINE_CHART_IMAGE from '../../../Assets/WINE_CHART.jpg';
+import PIZZAS_IMAGE from '../../../Assets/PIZZAS.jpg';
 
 export function groupMenuItemsByType(items) {
   return Object.values(items).reduce((results, item) => {
@@ -15,13 +16,14 @@ export function getMenuTypeIcon(type) {
   let icon;
   switch (type) {
     case 0:
+    default:
       icon = RestaurantMenu;
       break;
     case 1:
       icon = LocalBar;
       break;
-    default:
-      icon = RestaurantMenu;
+    case 2:
+      icon = LocalPizza;
       break;
   }
 
@@ -30,10 +32,12 @@ export function getMenuTypeIcon(type) {
 
 export function getMenuTypeImage(type) {
   switch (type) {
-    case 1:
-      return WINE_CHART_IMAGE;
     case 0:
     default:
       return FOOD_AND_DRINKS_IMAGE;
+    case 1:
+      return WINE_CHART_IMAGE;
+    case 2:
+      return PIZZAS_IMAGE;
   }
 }
